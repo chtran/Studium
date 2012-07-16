1bce3b3886241202f3b2bc16adc781ada102b990
Chau Tran <chau_tran@brown.edu>
2012-07-16 16:08:05 +0700

    x

--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -19,15 +19,18 @@ class PusherController < ApplicationController
     webhook = Pusher::WebHook.new(request)
     if webhook.valid?
       webhook.events.each do |event|
-        puts event["channel"]
-        room_id = event["channel"].split("presence-room_")[1].to_i
-        case event["name"]
-        when 'channel_occupied'
-          Room.find(room_id).update_attribute("active",true)
-        when 'channel_vacated'
-          room = Room.find(room_id)
-          room.update_attribute("active",false)
-          room.users = []
+        # If the channel is a rooms#join's channel
+        if event["channel"].match /^presence-room_(\d+)/
+          room_id = $1.to_i
+          case event["name"]
+          when 'channel_occupied'
+            Room.find(room_id).update_attribute("active",true)
+          when 'channel_vacated'
+            room = Room.find(room_id)
+            room.update_attribute("active",false)
+            room.users = []
+            puts "Deactivated room #{room_id}"
+          end
         end
       end
       render text: 'ok'
