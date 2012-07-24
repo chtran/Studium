2309a4a45904107c474b69550dd9f44346f5d955
Chau Tran <chau_tran@brown.edu>
2012-07-24 10:50:43 +0700

    Save cookie data using active record

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Studium::Application.config.session_store :cookie_store, key: '_studium_session'
+Studium::Application.config.session_store :active_record_store
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120719071303) do
+ActiveRecord::Schema.define(:version => 20120724034935) do
 
   create_table "category_types", :force => true do |t|
     t.string   "category_name"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "choices", :force => true do |t|
     t.integer  "question_id"
     t.text     "content"
     t.string   "choice_letter"
-    t.datetime "created_at",                       :null => false
-    t.datetime "updated_at",                       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean  "correct",       :default => false
   end
 
@@ -41,8 +41,8 @@ ActiveRecord::Schema.define(:version => 20120719071303) do
   create_table "paragraphs", :force => true do |t|
     t.text     "content"
     t.string   "title"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "permissions", :force => true do |t|
@@ -50,8 +50,8 @@ ActiveRecord::Schema.define(:version => 20120719071303) do
     t.integer  "user_id"
     t.integer  "thing_id"
     t.string   "thing_type"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "profiles", :force => true do |t|
@@ -60,16 +60,16 @@ ActiveRecord::Schema.define(:version => 20120719071303) do
     t.datetime "date_of_birth"
     t.string   "school"
     t.integer  "user_id"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "image"
     t.string   "link"
   end
 
   create_table "question_types", :force => true do |t|
     t.integer  "category_type_id"
-    t.datetime "created_at",                          :null => false
-    t.datetime "updated_at",                          :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "type_name"
     t.boolean  "need_paragraph",   :default => false
   end
@@ -77,8 +77,8 @@ ActiveRecord::Schema.define(:version => 20120719071303) do
   create_table "questions", :force => true do |t|
     t.text     "prompt"
     t.integer  "exp",              :default => 1400
-    t.datetime "created_at",                         :null => false
-    t.datetime "updated_at",                         :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "paragraph_id"
     t.integer  "question_type_id"
     t.string   "title"
@@ -107,6 +107,16 @@ ActiveRecord::Schema.define(:version => 20120719071303) do
     t.integer  "room_mode_id"
   end
 
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "",    :null => false
     t.string   "encrypted_password",     :default => "",    :null => false
@@ -118,8 +128,8 @@ ActiveRecord::Schema.define(:version => 20120719071303) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean  "admin",                  :default => false
     t.integer  "room_id"
     t.integer  "status",                 :default => 0
