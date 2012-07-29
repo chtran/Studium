bae21c12f9eeecedc75c59c084116fc6a5dba668
Kien Hoang <kienhoangdev@gmail.com>
2012-07-30 03:20:32 +0700

    Fixed reload room mode. Broke due to exp.

--- app/models/room_mode.rb
+++ app/models/room_mode.rb
@@ -17,7 +17,6 @@ class RoomMode < ActiveRecord::Base
     elsif self.namespace == "smart"
       profile_array=room.users.map {|user| user.profile}
       exp_array = profile_array.map {|profile| profile.exp}.sort
-      debugger
       min_exp = exp_array.first
       max_exp = exp_array.last
       output = unmastered_questions.where(exp: (min_exp-50)..(max_exp+50))
