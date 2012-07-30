b77cb48a1ffe4ac0511d23a7c7bc54afac05fd43
hoangtuananh <ahoang@bowdoin.edu>
2012-07-30 13:45:38 +0700

    merged

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,8 @@ class Message < ActiveRecord::Base
   validates  :body, :sender_id, :receiver_id, presence: true 
   attr_accessible  :body, :sender_id, :receiver_id
 
+  belongs_to :conversation
+
   def mes_valid?
     return self.sender_id != self.receiver_id
   end
