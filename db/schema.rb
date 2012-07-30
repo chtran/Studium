18d7958ae0ef436b8a1e3965981cd120dcf9faf8
Kien Hoang <kienhoangdev@gmail.com>
2012-07-30 11:05:16 +0700

    Added aws gem.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120729175252) do
+ActiveRecord::Schema.define(:version => 20120730034538) do
 
   create_table "badge_managers", :force => true do |t|
     t.integer  "user_id"
@@ -148,12 +148,16 @@ ActiveRecord::Schema.define(:version => 20120729175252) do
 
   create_table "questions", :force => true do |t|
     t.text     "prompt"
-    t.integer  "exp",              :default => 1400
-    t.datetime "created_at",                         :null => false
-    t.datetime "updated_at",                         :null => false
+    t.integer  "exp",                :default => 1400
+    t.datetime "created_at",                           :null => false
+    t.datetime "updated_at",                           :null => false
     t.integer  "paragraph_id"
     t.integer  "question_type_id"
     t.string   "title"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "questions_buffers", :force => true do |t|
