09e4bf86464be81e72f7fac2ead4130aa0e1386e
Kien Hoang <kienhoangdev@gmail.com>
2012-07-31 12:41:21 +0700

    Added heroku config vars to use s3.

--- app/models/question.rb
+++ app/models/question.rb
@@ -15,7 +15,7 @@ class Question < ActiveRecord::Base
   has_attached_file :image,
     storage: :s3,
     s3_credentials: {
-      bucket: ENV["S3_BUCKER_NAME"],
+      bucket: ENV["S3_BUCKET_NAME"],
       access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
   }
